be96b4b2ed37bdda2b57c773b43455c21d5f2206
skippi1 <skippi1@gmx.de>
2021-08-22 09:09:21 +0200

    update

--- scripts/split-yaml.awk
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/awk -f
-
-BEGIN { file=1; kind="unknown"; name="unknown" ; metadata=0 ; filename="" }
-/---/ { file=file+1; metadata=0 ; kind="unknown"; name="unknown" ; filename="" }
-/metadata:/ { metadata=1  }
-metadata==0 && /kind:(.*)/ { kind=$2 }
-metadata==1 && "unknown"==name && /name:(.*)/ { name=$2 ; metadata=0 }
-name!="unknown" && kind!="unknown" && filename=="" { filename=name"-"kind".yaml" ; print "mv "file".yaml "filename }
--- setup-ci.cmd
+++ setup-ci.cmd
@@ -1,8 +1,26 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.47.0/deploy/static/provider/cloud/deploy.yaml
+
+ipconfig.exe /flushdns
+
+SET SCRIPT_DIR=%~dp0
+
+REM install nginx ingress controller
+kubectl.exe apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.48.1/deploy/static/provider/cloud/deploy.yaml
+kubectl.exe wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
+
+REM install cert-manager
+kubectl.exe apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
+
+REM install argocd
+kubectl.exe create namespace argocd
+kubectl.exe apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl.exe apply -n argocd -f %SCRIPT_DIR%\kubernetes\argocd\argocd-ingress.yaml
+
+REM install tekton
+kubectl.exe apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+kubectl.exe apply --filename https://github.com/tektoncd/dashboard/releases/latest/download/tekton-dashboard-release.yaml
+kubectl.exe apply --filename kubernetes/tekton-pipelines/tekton-dashboard-ingress.yaml
+
+REM show argocd-password
+kubectl.exe -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
 
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl patch svc argocd-server -n argocd -p "{\"spec\":{\"type\":\"LoadBalancer\"}}"
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-kubectl apply -n argocd -f kubernetes/argocd
